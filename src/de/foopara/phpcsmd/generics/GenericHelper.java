f19c249d84dd56cddf65ea1c289754c7a400ea89
Norman Specht <webmaster@foopara.de>
2012-07-04 14:57:49 +0000

    prevent scanning symlinks

--- src/de/foopara/phpcsmd/generics/GenericHelper.java
+++ src/de/foopara/phpcsmd/generics/GenericHelper.java
@@ -108,4 +108,18 @@ public class GenericHelper {
         }
         return GenericHelper.phpcpdDistractor.getAbsolutePath();
     }
+
+    public static boolean isSymlink(File file) throws IOException {
+        if (file == null) {
+            throw new NullPointerException("File must not be null");
+        }
+        File canon;
+        if (file.getParent() == null) {
+            canon = file;
+        } else {
+            File canonDir = file.getParentFile().getCanonicalFile();
+            canon = new File(canonDir, file.getName());
+        }
+        return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());
+    }
 }
